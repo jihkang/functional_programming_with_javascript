13a568d3a83ec06e42daef778bf9626702c56bde
jihkang <106561135+jihkang@users.noreply.github.com>
2024-06-21 19:25:19 +0900

    Feat: reduce method

--- fp/fp.js
+++ fp/fp.js
@@ -1,107 +1,142 @@
 const users = [
-    { name: 'John', age: 34 },
-    { name: 'Amy', age: 20 },
-    { name: 'cam', age: 24 },
-    { name: 'Mike', age: 30 },
-    { name: 'Tom', age: 40 },
-]
+  { name: "John", age: 34 },
+  { name: "Amy", age: 20 },
+  { name: "cam", age: 24 },
+  { name: "Mike", age: 30 },
+  { name: "Tom", age: 40 },
+];
 
 const temp_users = [];
 
 /**
- *  
- * before created function 
+ *
+ * before created function
  *  to check data
  */
 
 for (let i = 0; i < users.length; ++i) {
-    if (users[i].age > 30) {
-        temp_users.push(users[i]);
-    }
+  if (users[i].age > 30) {
+    temp_users.push(users[i]);
+  }
 }
 
 const names = [];
 for (let i = 0; i < temp_users.length; ++i) {
-    const { name } = temp_users[i];
+  const { name } = temp_users[i];
 
-    names.push(name);
+  names.push(name);
 }
 /**
  *  remove duplication code with function
  */
 
 function _each(arr, iter) {
-    for (let i = 0;i < arr.length; ++i) {
-        iter(arr[i]);
-    }
-    return arr;
+  for (let i = 0; i < arr.length; ++i) {
+    iter(arr[i]);
+  }
+  return arr;
 }
 
 const _map = (arr, map) => {
-    const ret_arr = [];
-    _each(arr, (item) => ret_arr.push(map(item)));
-    return ret_arr;
-}
+  const ret_arr = [];
+  _each(arr, (item) => ret_arr.push(map(item)));
+  return ret_arr;
+};
 
 function _filter(arr, predict) {
-    const ret_arr = [];
-    _each(arr, (item) => {
-        if (typeof predict === 'function' && predict(item)) {
-            ret_arr.push(item);
-        }
-    });
-    return ret_arr;
+  const ret_arr = [];
+  _each(arr, (item) => {
+    if (typeof predict === "function" && predict(item)) {
+      ret_arr.push(item);
+    }
+  });
+  return ret_arr;
 }
 
 function _curry(fn) {
-    return function(a, b) {
-        return arguments.length === 2? fn(a, b) : function(b) {
-            return fn(a, b);
-        }
-    }
+  return function (a, b) {
+    return arguments.length === 2
+      ? fn(a, b)
+      : function (b) {
+          return fn(a, b);
+        };
+  };
 }
 
 function _curryr(fn) {
-    return function(a, b) {
-        return arguments.length === 2? fn(a, b) : function (b) {
-            return fn(b, a);
-        }
-    }
+  return function (a, b) {
+    return arguments.length === 2
+      ? fn(a, b)
+      : function (b) {
+          return fn(b, a);
+        };
+  };
 }
 
-const _get = _curryr(function(obj, key) {
-    console.log(obj, key);
-    return obj == null ? undefined : obj[key];
-    
+const _get = _curryr(function (obj, key) {
+  console.log(obj, key);
+  return obj == null ? undefined : obj[key];
 });
 
-function test() {
 /**
- *  test _map, _filter function 
+ * pipe function
+ * 여러 함수를 인자로 받아 연속적으로 실행해주는 함수.
  */
-    // console.log(temp_users);
-    // console.log(names);
-    // console.log(_map(users, (user) => ({
-    //     ...user,
-    //     age: user.age + 30
-    // })), users);
-    // console.log(users);
-    // console.log(_filter(users, (user) => user.age));
-    const get_age = _get('age');
-    console.log(get_age(users[0]))
-    console.log(_map(_filter(users, (user) => user.age < 40), (user) => user.age));
-    console.log(_map(_filter(users, (user) => user.age < 40), _get('age')));
-    console.log(_map(document.querySelectorAll("*"), function(node) {
-        return node.nodeName;
-    }))
-    const curry = _curry((a, b) => a + b);
-    console.log(curry(10)(5));
-    const curry10 = curry(10);
-    console.log(curry10(5));
-    const curryr = _curryr((a, b) => a - b);
-    const sub10 = curryr(10);
-    console.log(curryr(5, 10));
-    console.log(curryr(10)(5));
+
+function _rest(list, num) {
+  return Array.prototype.slice.call(list, num);
+}
+
+function _reduce(arr, apply, init) {
+  arr = _rest(arr, init ? 0 : 1);
+  _each(arr, (item) => {
+    init = apply(init, item);
+  });
+  return init;
+}
+
+function test() {
+  /**
+   *  test _map, _filter function
+   */
+  // console.log(temp_users);
+  // console.log(names);
+  // console.log(_map(users, (user) => ({
+  //     ...user,
+  //     age: user.age + 30
+  // })), users);
+  // console.log(users);
+  // console.log(_filter(users, (user) => user.age));
+  const get_age = _get("age");
+  console.log(get_age(users[0]));
+  console.log(
+    _map(
+      _filter(users, (user) => user.age < 40),
+      (user) => user.age
+    )
+  );
+  console.log(
+    _map(
+      _filter(users, (user) => user.age < 40),
+      _get("age")
+    )
+  );
+  console.log(
+    _map(document.querySelectorAll("*"), function (node) {
+      return node.nodeName;
+    })
+  );
+  const curry = _curry((a, b) => a + b);
+  console.log(curry(10)(5));
+  const curry10 = curry(10);
+  console.log(curry10(5));
+  const curryr = _curryr((a, b) => a - b);
+  const sub10 = curryr(10);
+  console.log(curryr(5, 10));
+  console.log(curryr(10)(5));
+
+  console.log(_reduce([1, 2, 3, 4], (a, b) => a + b, 30));
+  console.log(_reduce([1, 2, 3, 4], (a, b) => a + b, 20));
 }
 
 test();
